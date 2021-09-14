5b68a11afac58ad74a627230edcd3bff559b8ae6
RamiZregat <rami.zregat97@gmail.com>
2021-09-14 21:39:22 +0300

    done lab 13

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const server=express();
 server.use(cors());
 const PORT=process.env.PORT;
+server.use(express.json());
 
 const mongoose = require('mongoose');
 
@@ -20,7 +21,6 @@ async function main() {
     description: String,
     status: String,
     email: String,
-    posterURL: String
   });
    Book = mongoose.model('Book', BookSchema);
   
@@ -33,40 +33,37 @@ async function  seedData(){
         description:'Right from examining the division of labour, the origin and use of money and the division of stock, to the rise and progress of cities and towns after the fall of the Roman Empire, the systems of political economy and the taxes on various private revenues, Adam Smith’s The Wealth of Nations provides an extensive assessment of the creation of a nation’s wealth.',
         status:'In stock.',
         email:'rami.zregat97@gmail.com',
-        posterURL:'https://images-na.ssl-images-amazon.com/images/I/510PLv185uS._SX460_BO1,204,203,200_.jpg'
     })
     const Book2=new Book({
         title:'Make Your Own Luck',
         description:'How to Increase Your Odds of Success in Sales, Startups, Corporate Career and Life',
         status:'Out of stock',
         email:'rami.zregat97@gmail.com',
-        posterURL:'https://images-na.ssl-images-amazon.com/images/I/41GhlX5jY1L._SX460_BO1,204,203,200_.jpg'
     })
     const Book3=new Book({
         title:'The Law of Success in Sixteen Lessons ',
         description:'From helping you rid yourself of aimlessness and find a definite purpose to teaching you the art of negotiating harmoniously with others, this course on the fundamentals of success will guide you, lesson by lesson, through the sixteen laws of success.',
         status:'In stock',
         email:'rami.zregat97@gmail.com',
-        posterURL:'https://images-na.ssl-images-amazon.com/images/I/41Kq6PplOXS._SX460_BO1,204,203,200_.jpg'
     })
     await Book1.save();
     await Book2.save();
     await Book3.save();
 }
 
-server.get('/', homeHandler);
-// http://localhost:3010/books
+
+// http://localhost:3010/books?emailuser=email
 server.get('/books',getBooksHandler)
+// http://localhost:3010/addbooks
+server.post('/addbooks',addBooksHandler)
+// http://localhost:3010/deletebooks/id?email=email
+server.delete('/deletebooks/:id',deleteBooksHandler)
 
-
-function homeHandler(req,res){
-
-    res.send('Home Page');
-}
 
 
 function getBooksHandler(req,res){
-Book.find({},(err,result)=>{
+    const emailuser=req.query.email;
+Book.find({email:emailuser},(err,result)=>{
     if(err)
     {
         console.log(err);
@@ -78,6 +75,49 @@ Book.find({},(err,result)=>{
 })
 
 }
+
+
+function addBooksHandler(req,res){
+    const {title,description,status,email}=req.body;
+    // const title = req.body.title
+    // const description = req.body.description
+    // const status = req.body.status
+    // const email = req.body.email
+    Book.create({title,description,status,email})
+    // title:title,
+    // description:description,
+    // status:status,
+    // email:email,
+    Book.find({email:email},(err,result)=>{
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            res.send(result);
+        }
+    })
+}
+
+function deleteBooksHandler(req,res){
+    const id=req.params.id
+    const emailuser=req.query.email
+
+    Book.deleteOne({_id:id},(err,result)=>{
+        Book.find({email:emailuser},(err,result)=>{
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            res.send(result);
+        }
+    })
+    })
+}
+
+
 server.listen(PORT, () => {
 console.log(`listening on ${PORT}`);
 })
+
